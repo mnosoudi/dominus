8dc13842f3165895e82e438ed12d0682999732bc
alexander.sokolov <alexandersokolov17@gmail.com>
2017-03-15 08:59:16 -0300

    Modified some tests + Added empty methods to signup validate
    -

--- src/main/java/com/dominus/dominus/SignupValidate.java
+++ src/main/java/com/dominus/dominus/SignupValidate.java
@@ -1,5 +1,48 @@
 package com.dominus.dominus;
 
-public class SignupValidate {
-
+public class SignupValidate 
+{
+	
+	//This function will call all of the methods to validate
+	public boolean validate(String name)
+	{
+		return false;
+	}
+	
+	
+	public boolean testLength(String name)
+	{
+		return false;
+	}
+	
+	public boolean testLetters(String name)
+	{
+		return false;
+	}
+	
+	public boolean testAt(String email)
+	{
+		return false;
+	}
+	
+	public boolean testPeriod(String email)
+	{
+		return false;
+	}
+	
+	public boolean passwordNull(String password)
+	{
+		return false;
+	}
+	
+	public boolean passwordEmpty(String password)
+	{
+		return false;
+	}
+	
+	public boolean passwordSize(String password)
+	{
+		return false;
+	}
+	
 }
