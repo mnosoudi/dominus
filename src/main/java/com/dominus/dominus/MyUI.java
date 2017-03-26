a71df144b8ccaa242f1886d96c7f8d758afc60c3
rdpi <ryanpitts95@gmail.com>
2017-03-26 20:41:59 -0300

    Database Base
    Pair Programming with Kelvin

--- src/main/java/com/dominus/dominus/MyUI.java
+++ src/main/java/com/dominus/dominus/MyUI.java
@@ -1,7 +1,13 @@
 package com.dominus.dominus;
 
+import java.util.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.annotation.WebServlet;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Map;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -17,6 +23,12 @@ public class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         setContent(new MainLayout());
+        HashMap<String, String> properties = new HashMap<String, String>();
+        properties.put("javax.persistence.jdbc.user", "sa");
+        properties.put("javax.persistence.jdbc.password", "sa");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+            "objectdb://localhost:6136/myDbFile.odb", properties);
+        EntityManager em = emf.createEntityManager();
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
