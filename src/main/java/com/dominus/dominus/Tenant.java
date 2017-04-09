361b8a5dab4678bcc66e73fc297dd6a2266f7e71
adamwoodland <adamwoodland@icloud.com>
2017-04-09 18:08:26 -0300

    Set up the project for JPA.

---
+++ src/main/java/com/dominus/dominus/Tenant.java
@@ -0,0 +1,20 @@
+package com.dominus.dominus;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tenant {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private int rating;
+	private String password;
+	private String email;
+	
+	
+}
